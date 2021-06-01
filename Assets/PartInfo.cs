9091c960194072902fac069091144500c761876a
Suleyman Enes Yavuz <enesy375@gmail.com>
2021-06-01 14:14:03 +0300

    Particles

---
+++ Assets/PartInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartInfo : MonoBehaviour
+{
+    
+    // 0 - boya // 1- spoiler ve ust body //2-tampon ve jant //3- repair // 4- motor // 5- turbo// 6- deccccccal 
+    public int partID;
+    public ParticleSystem partPickupParticle;
+    public bool showedOnce;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Makes sure that it works only once when gameobject activated
+        if (gameObject.activeInHierarchy && !showedOnce)
+        {
+            partPickupParticle.Play();
+            showedOnce = true;
+        }
+    }
+}
---
+++ Assets/PartPickUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PartPickUp : MonoBehaviour
+{
+
+    public GameObject carBody;
+    public GameObject currentSpoiler;
+    public GameObject currentRoofParts;
+    public GameObject currentDeccal;
+    public GameObject currentEngine;
+    public GameObject repair;
+    public ParticleSystem carSmoke;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Part"))
+        {
+            
+            if (other.gameObject.GetComponent<PartInfo>().partID == 1)
+            {
+                currentSpoiler.gameObject.SetActive(true);
+                Destroy(other.gameObject);
+            }
+            
+        }
+    }
+}
