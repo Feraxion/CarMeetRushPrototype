a93a2b8ec967e9b3e4e082466c7418a24db3162f
Suleyman Enes Yavuz <enesy375@gmail.com>
2021-06-02 10:54:44 +0300

    S

--- Assets/PartPickUp.cs
+++ Assets/PartPickUp.cs
@@ -11,9 +11,27 @@ public class PartPickUp : MonoBehaviour
     public GameObject currentSpoiler;
     public GameObject currentRoofParts;
     public GameObject currentDeccal;
+<<<<<<< Updated upstream
     public GameObject currentEngine;
     public GameObject repair;
     public ParticleSystem carSmoke;
+=======
+
+    public GameObject currentBumpers;
+    public GameObject currentTires;
+    public GameObject currentTires2;
+    public GameObject currentTires3;
+    public GameObject currentTires4;
+
+
+    public bool exhaustUpgradedOnce;
+
+    //public GameObject repair;
+    public ParticleSystem carSmokeLevel0;
+    public ParticleSystem carSmokeLevel1;
+    public ParticleSystem carSmokeLevel2;
+
+>>>>>>> Stashed changes
     
     
     // Start is called before the first frame update
@@ -35,8 +53,70 @@ public class PartPickUp : MonoBehaviour
             
             if (other.gameObject.GetComponent<PartInfo>().partID == 1)
             {
+<<<<<<< Updated upstream
                 currentSpoiler.gameObject.SetActive(true);
                 Destroy(other.gameObject);
+=======
+                // 0 - boya // 1- spoiler ve ust body //2-tampon ve jant //3- repair // 4- motor // 5- turbo// 6- deccccccal 
+
+                case 5:
+                    if (exhaustUpgradedOnce)
+                    {
+                        carSmokeLevel1.gameObject.SetActive(false);
+                        carSmokeLevel2.gameObject.SetActive(true);
+                        Destroy(other.gameObject);
+
+                    }
+                    else
+                    {
+                        carSmokeLevel0.gameObject.SetActive(false);
+                        carSmokeLevel1.gameObject.SetActive(true);
+                        Destroy(other.gameObject);
+
+
+                    }
+
+                    carBody.GetComponent<PlayerMovement>().movementSpeed += 2;
+                    break;
+                case 4:
+                    if (exhaustUpgradedOnce)
+                    {
+                        carSmokeLevel1.gameObject.SetActive(false);
+                        carSmokeLevel2.gameObject.SetActive(true);
+                        Destroy(other.gameObject);
+
+                    }
+                    else
+                    {
+                        carSmokeLevel0.gameObject.SetActive(false);
+                        carSmokeLevel1.gameObject.SetActive(true);
+                        Destroy(other.gameObject);
+
+                    }
+                    carBody.GetComponent<PlayerMovement>().movementSpeed += 2;
+
+                    break;
+                case 3:
+                    
+                    break;
+                case 2:
+                    currentBumpers.gameObject.SetActive(true);
+                    currentTires.gameObject.SetActive(true);
+                    currentTires2.gameObject.SetActive(true);
+                    currentTires3.gameObject.SetActive(true);
+                    currentTires4.gameObject.SetActive(true);
+                    
+                    Destroy(other.gameObject);
+                    break;
+                case 1:
+                    currentSpoiler.gameObject.SetActive(true);
+                    currentRoofParts.gameObject.SetActive(true);
+                    Destroy(other.gameObject);
+                    break;
+                default:
+                    print ("Incorrect intelligence level.");
+                    break;
+>>>>>>> Stashed changes
             }
             
         }
